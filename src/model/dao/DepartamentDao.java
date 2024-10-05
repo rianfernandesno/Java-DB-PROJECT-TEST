a594ccef4c8123614cb0391d89fbb8e8d3b7183a
rianfernandesno <zrpapz12@gmail.com>
2024-10-04 23:29:01 -0300

    Criação do package DB e suas classes EXCPETIONS && Connection Departametn CLASS Seller Selle && Depatament both DAO

---
+++ src/model/dao/DepartamentDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import model.entities.Departament;
+
+import java.util.List;
+
+public interface DepartamentDao {
+    void insert(Departament obj);
+    void update(Departament obj);
+    void deleteById(Integer id);
+    Departament findByInd(Integer id);
+    List<Departament> findAll();
+}
