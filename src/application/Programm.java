078678fde5791b2241e950e9b11772be5c6c3aa6
rianfernandesno <zrpapz12@gmail.com>
2024-10-04 23:21:55 -0300

    Criação do package DB e suas classes EXCPETIONS && Connection Departametn CLASS Seller

--- src/application/Programm.java
+++ src/application/Programm.java
@@ -1,11 +1,17 @@
 package application;
 
 import model.entities.Departament;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Programm {
     public static void main(String[] args) {
 
         Departament obj = new Departament(1, "Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21, "bob", "bob@gmail.com",new Date(), 3000,obj);
+
+        System.out.println(seller);
     }
 }
